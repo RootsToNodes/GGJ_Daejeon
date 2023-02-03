2ef5c26988ef1d6ddd7e4908179265b92ad290b1
BangSeokHyun <stuban175@gmail.com>
2023-02-04 02:35:40 +0900

    임시 게임 매니저 추가 및 노드 생성 로직 추가

---
+++ Assets/Scripts/Managers/GameManager.cs
@@ -0,0 +1,31 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    [SerializeField] private LayerMask nodeLayerMask;
+    [SerializeField] private Tree tree;
+
+    private void Update()
+    {
+        CheckInput();
+    }
+
+    private void CheckInput()
+    {
+        if (Input.GetMouseButtonUp(0))
+        {
+            RaycastHit2D hit = Physics2D.Raycast(Camera.main.ScreenToWorldPoint(Input.mousePosition), Vector2.zero,
+                float.PositiveInfinity, nodeLayerMask);
+
+            if (hit != null && hit.collider != null && hit.collider.TryGetComponent(typeof(Node), out var node))
+            {
+                //일단 대충 터치만하면 2개 만듬
+                tree.CreateNewNode((Node) node, new NodeStatus());
+                tree.CreateNewNode((Node) node, new NodeStatus());
+            }
+        }
+    }
+}
