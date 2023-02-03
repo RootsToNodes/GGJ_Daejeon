d446e6ec126c4bdfdffd380c1b4307be11b5b530
BangSeokHyun <stuban175@gmail.com>
2023-02-03 22:30:07 +0900

    기본 스크립트 추가

---
+++ Assets/Scripts/Tree.cs
@@ -0,0 +1,22 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Tree : MonoBehaviour
+{
+    public Node originPrefab; //임시
+    
+    public Node roots { get; private set; }
+
+    private void Start()
+    {
+        CreateNewNode(null);
+    }
+
+    public void CreateNewNode(Node parent)
+    {
+        roots = Instantiate(originPrefab);
+        roots.Initialization(parent, this);
+    }
+}
