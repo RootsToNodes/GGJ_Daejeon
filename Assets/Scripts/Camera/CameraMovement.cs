d9601edbe3e7db42300a05587963d8bbd920ac84
BangSeokHyun <stuban175@gmail.com>
2023-02-04 20:12:27 +0900

    카메라 작업 중

---
+++ Assets/Scripts/Camera/CameraMovement.cs
@@ -0,0 +1,28 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraMovement : MonoBehaviour
+{
+    [SerializeField] private float dragSpeed;
+
+    private Vector2 touchStart;
+    
+    private void LateUpdate()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            
+        }
+
+        if (Input.GetMouseButton(0))
+        {
+            
+        }
+        if (Input.GetMouseButtonUp(0))
+        {
+            
+        }
+    }
+}
